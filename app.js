bba5afe972277aa3f1652ba0d72c3a296d5516a9
Eduardo Valencia <EduardoVG@proton.me>
2024-03-22 12:20:30 -0600

    Versión 1.2

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
 let playSonido = (tecla) => {
-document.getElementById(tecla).play();
+    document.querySelector(tecla).play();
+}
+
+for(let counter = 0; counter < listaDeTeclas.length ; counter++) {
+    const tecla = listaDeTeclas[counter];
+    const instrumento = tecla.classList[1];
+
+    const idAudio = `#sonido_${instrumento}`;
+    console.log(idAudio)
+    console.log(counter)
+
+    tecla.onclick = function () {
+        playSonido(idAudio);
+    };
 }
